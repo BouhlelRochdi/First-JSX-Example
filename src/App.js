49ff25b32ebb1145e7389c6ae7f6161e81013b61
Rochdi Bouhlel <rochdi.bouhlel@hotmail.fr>
2021-11-27 10:12:57 +0100

    Revert "first commit"
    This reverts commit 59fa5d62be53d27670ce44a29b523928e6c7b981.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
-import  FullName  from "./component/profile/FullName";
-import  Address  from "./component/profile/Address";
-import  ProfilePhoto  from "./component/profile/ProfilePhoto";
 
 function App() {
   return (
-    <div >
-      this is my name : <FullName/>
-      adress : <Address/>
-      photo: <ProfilePhoto/>
+    <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+      <h1 className="title red">Your name here</h1>
+      <br />
+      <img src="/imageInSrc.jpg" alt="" />
+      <br />
+      <img src={"/imageInPublic.jpg"} alt="" />
+      <video width="320" height="240" controls>
+        <source src="myVideo.mp4" type="video/mp4" />
+      </video>
     </div> 
   );
 }
+
 export default App;
